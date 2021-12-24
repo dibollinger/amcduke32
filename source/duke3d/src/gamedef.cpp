0fcf6b926bc208eabc1970014335ea2d60dfcaf8
Dino Bollinger <dino.bollinger@gmail.com>
2021-12-24 01:53:40 +0100

    AMC: Update branding

--- source/duke3d/src/gamedef.cpp
+++ source/duke3d/src/gamedef.cpp
@@ -6508,8 +6508,8 @@ void C_Compile(const char *fileName)
         if (g_loadFromGroupOnly == 1 || numgroupfiles == 0)
         {
 #if defined(AMC_BUILD)
-            Bsprintf(tempbuf,"Required game data was not found.  This binary is only intended to be run in a valid AMC TC install folder!\n\n"
-                     "You must copy this binary to your AMC TC directory before continuing!");
+            Bsprintf(tempbuf,"Required game data was not found.  This binary is only intended to be run in a valid AMC Squad install folder!\n\n"
+                     "You must copy this binary to your AMC Squad directory before continuing!");
             G_GameExit(tempbuf);
 #elif !defined(EDUKE32_STANDALONE)
             char const *gf = G_GrpFile();
