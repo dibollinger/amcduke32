nukeykt/NBlood

Engine: Factor a new vox_clearid() out from vox_undefine()

source/build/include/build.h
@@ -1251,6 +1251,7 @@ void    artConvertRGB(palette_t *pic, uint8_t const *buf, int32_t bufsizx, int32
 void    tileUpdatePicSiz(int32_t picnum);
 
 int32_t   qloadkvx(int32_t voxindex, const char *filename);
+void vox_clearid(int32_t const);
 void vox_undefine(int32_t const);
 intptr_t   tileCreate(int16_t tilenume, int32_t xsiz, int32_t ysiz);
 void   tileCopySection(int32_t tilenume1, int32_t sx1, int32_t sy1, int32_t xsiz, int32_t ysiz, int32_t tilenume2, int32_t sx2, int32_t sy2);

source/build/src/engine.cpp
@@ -12293,9 +12293,8 @@ int32_t qloadkvx(int32_t voxindex, const char *filename)
     return 0;
 }
 
-void vox_undefine(int32_t const tile)
+void vox_clearid(int32_t const voxindex)
 {
-    ssize_t voxindex = tiletovox[tile];
     if ((unsigned)voxindex >= MAXVOXELS)
         return;
 
@@ -12316,9 +12315,14 @@ void vox_undefine(int32_t const tile)
     }
     voxscale[voxindex] = 65536;
     voxflags[voxindex] = 0;
-    tiletovox[tile] = -1;
 
     // TODO: nextvoxid
+}
+
+void vox_undefine(int32_t const tile)
+{
+    vox_clearid(tiletovox[tile]);
+    tiletovox[tile] = -1;
 }
 
 //

dibollinger/amcduke32

source/build/include/build.h
@@ -1260,6 +1260,7 @@ void    artConvertRGB(palette_t *pic, uint8_t const *buf, int32_t bufsizx, int32
 void    tileUpdatePicSiz(uint16_t picnum);
 
 int32_t   qloadkvx(int32_t voxindex, const char *filename);
+void vox_clearid(uint16_t const);
 void vox_undefine(uint16_t const);
 intptr_t   tileCreate(uint16_t tilenume, int32_t xsiz, int32_t ysiz);
 void   tileCopySection(uint16_t tilenume1, int32_t sx1, int32_t sy1, int32_t xsiz, int32_t ysiz, uint16_t tilenume2, int32_t sx2, int32_t sy2);

source/build/src/engine.cpp
@@ -12344,9 +12344,8 @@ int32_t qloadkvx(int32_t voxindex, const char *filename)
     return 0;
 }
 
-void vox_undefine(uint16_t const tile)
+void vox_clearid(int32_t const voxindex)
 {
-    ssize_t voxindex = tiletovox[tile];
     if ((unsigned)voxindex >= MAXVOXELS)
         return;
 
@@ -12367,9 +12366,14 @@ void vox_undefine(uint16_t const tile)
     }
     voxscale[voxindex] = 65536;
     voxflags[voxindex] = 0;
-    tiletovox[tile] = -1;
 
     // TODO: nextvoxid
+}
+
+void vox_undefine(uint16_t const tile)
+{
+    vox_clearid(tiletovox[tile]);
+    tiletovox[tile] = -1;
 }
 
 //